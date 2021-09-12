cf531ce81cb91c31f8362ecd48bca3788416b5b3
Aviansh <avinashtalk2me@gmail.com>
2021-09-12 22:46:13 +0530

    config changes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `markerting@${domain}/marketing/remoteEntry.js`,
+        marketing: `markerting@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
