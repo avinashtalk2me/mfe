c2e2167103f360b3570120ddb50d343e08fda763
Aviansh <avinashtalk2me@gmail.com>
2021-09-12 16:29:22 +0530

    changes to the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <div>
-      <h1>Container</h1>
-      <MarketingApp/>
-  </div>
+  return (<div>
+    <h1>Container</h1>
+    <MarketingApp/>
+</div>)
 };
