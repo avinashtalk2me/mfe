aea0ca2304839805c6b935be1c2c35a2a30ab5e1
Aviansh <avinashtalk2me@gmail.com>
2021-09-13 21:02:52 +0530

    added style provider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ca",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
